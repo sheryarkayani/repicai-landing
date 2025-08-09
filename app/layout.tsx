c2a6e41958dc39a6a13a63cc301c22e4220164f2
sheryarkayani <sheryarali2010@gmail.com>
2025-08-09 16:24:06 +0500

    updated favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,8 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'Clara voice AI',
-  description: 'Created with v0',
+  title: 'Repic AI',
+  description: 'Created by Repic',
   generator: 'v0.dev',
 }
 
